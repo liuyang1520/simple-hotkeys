bd9808f1a1967a5f7ec03a90dc4aff41eda5c8b4
liuyang1520 <liuyang1520@gmail.com>
2019-03-20 01:31:59 -0600

    Fix css

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,7 +1,17 @@
 'use strict';
 
-let globalSwitch = document.querySelector('.global-switch input');
+let container = document.querySelector('.each-item-switch');
 let switches = document.querySelectorAll('.each-item-switch input');
+
+chrome.storage.sync.get('disabledKeycode', (result) => {
+  let codes = result['disabledKeycode']
+  if (!Array.isArray(codes)) return;
+  for (let keycode of codes) {
+    let checkbox = document.querySelector(`label[data-keycode="${keycode}"] > input`);
+    checkbox.checked = false;
+  }
+  setTimeout(() => container.classList.remove('notransition'), 500);
+})
 
 for (let element of switches) {
   element.onclick = () => {
