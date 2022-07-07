09249b4c47f4218cf88e73a26651a287f947e6e6
liuyang1520 <liuyang1520@gmail.com>
2022-07-07 00:23:08 -0700

    try to use chrome commands to rewrite

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -33,3 +33,88 @@ chrome.runtime.onMessage.addListener((request, sender, _sendResponse) => {
     return true;
   }
 });
+
+const tabs = {};
+
+function switchToPreviouslyActiveTab() {
+}
+
+function removeTab(tabId, windowId) {
+  tabs[windowId] = tabs[windowId].filter(id => id !== tabId);
+  if (tabs[windowId].length === 0) {
+    delete tabs[windowId];
+  }
+}
+
+chrome.tabs.onActivated.addListener(({ tabId, windowId }) => {
+  if (!tabs[windowId] || !tabs[windowId].length) {
+    tabs[windowId] = [];
+  }
+  tabs[windowId].unshift(tabId);
+  tabs[windowId] = tabs[windowId].filter((id, idx, arr) => arr.indexOf(id) === idx);
+});
+
+chrome.tabs.onRemoved.addListener((tabId, { windowId }) => {
+  removeTab(tabId, windowId);
+});
+
+chrome.tabs.onDetached.addListener((tabId, { oldWindowId }) => {
+  removeTab(tabId, oldWindowId);
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "search-in-new-active-tab") {
+
+  } else if (command === "search-in-new-background-tab") {
+
+  } else if (command === "pin-unpin-tab") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      const activeTab = tabs[0];
+      const activeTabId = activeTab.id;
+      const activeTabPinned = activeTab.pinned;
+      chrome.tabs.update(activeTabId, {pinned: !activeTabPinned});
+    });
+  } else if (command === "extract-tab-to-new-window") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      const activeTab = tabs[0];
+      const activeTabId = activeTab.id;
+      chrome.windows.create({tabId: activeTabId})
+    });
+  } else if (command === "navigate-to-last-active-tab") {
+    chrome.windows.getCurrent({}, ({ id: windowId }) => {
+      if (tabs[windowId].length > 1) {
+        const lastTab = tabs[windowId][1];
+        tabs[windowId] = tabs[windowId].filter(id => id !== lastTab);
+        chrome.tabs.update(lastTab, { active: true });
+      }
+    });
+  } else if (command === "navigate-back-to-last-active-tab-in-stack") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(_tabs) {
+      const activeTab = _tabs[0];
+      const activeTabId = activeTab.id;
+      const windowId = activeTab.windowId;
+      for (let i = 0; i < tabs[windowId].length; i++) {
+        if (tabs[windowId][i] === activeTabId && i > 0) {
+          tabs[windowId] = tabs[windowId].filter(id => id !== activeTabId);
+          chrome.tabs.update(tabs[windowId][i - 1], { active: true });
+          return;
+        }
+      }
+    });
+  } else if (command === "navigate-forward-to-last-active-tab-in-stack") {
+    chrome.tabs.query({active: true, currentWindow: true}, function(_tabs) {
+      const activeTab = _tabs[0];
+      const activeTabId = activeTab.id;
+      const windowId = activeTab.windowId;
+      for (let i = 0; i < tabs[windowId].length; i++) {
+        if (tabs[windowId][i] === activeTabId && i + 1 < tabs[windowId].length) {
+          tabs[windowId] = tabs[windowId].filter(id => id !== activeTabId);
+          chrome.tabs.update(tabs[windowId][i + 1], { active: true });
+          return;
+        }
+      }
+    });
+  } else {
+
+  }
+});
